18c3026a6fdb0323d9a67faca47e868f9b21ce71
Your Name <you@example.com>
2024-03-04 21:24:00 +0600

    updated code added new files

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,30 @@
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm'
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const TransformationsTypePage = () => {
+const TransformationsTypePage = async ({params : {type}}: SearchParamProps) => {
+  
+  const { userId} = auth();
+
+  if(!userId) redirect("/sign-in")
+
+  const user = await getUserById(userId)
+
+  const transformation = transformationTypes[type]
   return (
-    <div>TransformationsTypePage</div>
+    <>
+    
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+    <TransformationForm action='Add' userId={user._id}
+    type={transformation.type as TransformationTypeKey}
+    creditBalance={user.creditBalance}
+    />
+    </>
   )
 }
 
